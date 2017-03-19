75dfbefc1be0fc43053293367a6b283b16acdedc
Diego <digo_cb@hotmail.com>
2017-03-19 17:55:50 -0300

    Corrige indentação de´códigos

--- ConsoleAppCarrinhoCompras/src/diego/ferreira/Produto.java
+++ ConsoleAppCarrinhoCompras/src/diego/ferreira/Produto.java
@@ -1,15 +1,15 @@
 package diego.ferreira;
 public class Produto {
- String nome;
- float preco;
- public Produto(String string, float d) {
-  this.nome = string;
-  this.preco = d;
- };
- public String Nome() {
-  return this.nome;
- }
- public float Preco() {
-  return this.preco;
- }
+    String nome;
+    float preco;
+    public Produto(String string, float d) {
+        this.nome = string;
+        this.preco = d;
+    };
+    public String Nome() {
+        return this.nome;
+    }
+    public float Preco() {
+        return this.preco;
+    }
 }
